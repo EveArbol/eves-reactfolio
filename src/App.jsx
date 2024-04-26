efec4dd533f9d179c706a9ed723a2c9661f3c6e3
Evelin Arboleda <evelinarboleda23@gmail.com>
2024-04-26 10:50:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1>Eves ReactFolio</h1>
+      
+    </>
+  )
+}
+
+export default App
